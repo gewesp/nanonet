3bfe916a9cc2ddfbfc2f715e2be371b899ea6e05
Gerhard Wesp <gewesp@gmail.com>
2025-06-08 12:05:17 +0200

    MOD: Consolidate tests

--- scripts/run-all-tests.sh
+++ scripts/run-all-tests.sh
@@ -1,11 +1,9 @@
 #! /bin/sh
 
-. scripts/testenv.sh
+. ./scripts/testenv.sh
 
 ./scripts/run-fast-tests.sh
-./scripts/run-annoying-tests.sh
 
-$BIN_DIR/spatial-index-test                 > $GOLDEN_DIR/spatial-index.txt
 $BIN_DIR/dispatch-test                      > $GOLDEN_DIR/dispatch.txt
 
-./check-git-diffs.sh
+./scripts/check-git-diffs.sh
